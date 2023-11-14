8b39bf10f54680468732f83944f259d57140043d
svasenkov <aanher@gmail.com>
2023-11-14 19:10:01 +0100

    added status tests

---
+++ src/test/java/tests/StatusTests.java
@@ -0,0 +1,76 @@
+package tests;
+
+import org.junit.jupiter.api.Test;
+import static io.restassured.RestAssured.get;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.hasKey;
+import static org.hamcrest.Matchers.is;
+
+public class StatusTests {
+    /*
+    1. Make request to https://selenoid.autotests.cloud/status
+    2. Get response { total: 20, used: 0, queued: 0, pending: 0, browsers: ...
+    3. Check total is 20
+     */
+    @Test
+    void checkTotalMini() {
+        get("https://selenoid.autotests.cloud/status")
+                .then()
+                .body("total", is(20));
+    }
+
+    @Test
+    void checkTotal() {
+        given()
+                //
+                .when()
+                .get("https://selenoid.autotests.cloud/status")
+                .then()
+                .body("total", is(20));
+    }
+
+    @Test
+    void checkTotalWithLogs() {
+        given()
+//                .log().all()
+                .log().uri()
+                .log().method()
+                .when()
+                .get("https://selenoid.autotests.cloud/status")
+                .then()
+//                .log().all()
+                .log().status()
+                .log().body()
+                .body("total", is(20));
+    }
+
+    @Test
+    void checkTotalWithStatus() {
+        given()
+                .log().uri()
+                .log().method()
+                .when()
+                .get("https://selenoid.autotests.cloud/status")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(200)
+                .body("total", is(20));
+    }
+
+    @Test
+    void checkTotalAndChrome() {
+        given()
+                .log().uri()
+                .log().method()
+                .when()
+                .get("https://selenoid.autotests.cloud/status")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(200)
+                .body("total", is(20))
+                .body("browsers.chrome", hasKey("100.0"));
+    }
+
+}
