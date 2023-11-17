8f130308c891e950f8b523d9ab323eb9838c3171
svasenkov <aanher@gmail.com>
2023-11-17 19:08:33 +0100

    added allure

--- src/test/java/tests/LoginExtendedTests.java
+++ src/test/java/tests/LoginExtendedTests.java
@@ -1,11 +1,14 @@
 package tests;
 
+import io.qameta.allure.restassured.AllureRestAssured;
 import models.lombok.LoginBodyLombokModel;
 import models.lombok.LoginResponseLombokModel;
 import models.pojo.LoginBodyPojoModel;
 import models.pojo.LoginResponsePojoModel;
 import org.junit.jupiter.api.Test;
 
+import static helpers.CustomAllureListener.withCustomTemplates;
+import static io.qameta.allure.Allure.step;
 import static io.restassured.RestAssured.given;
 import static io.restassured.http.ContentType.JSON;
 import static org.hamcrest.Matchers.is;
@@ -77,6 +80,82 @@ public class LoginExtendedTests {
 
         assertEquals("QpwL5tke4Pnpja7X4", response.getToken());
     }
+
+    @Test
+    void successfulLoginWithAllureTest() {
+        LoginBodyLombokModel authBody = new LoginBodyLombokModel();
+        authBody.setEmail("eve.holt@reqres.in");
+        authBody.setPassword("cityslicka");
+
+        LoginResponseLombokModel response =  given()
+                .filter(new AllureRestAssured())
+                .log().uri()
+                .log().method()
+                .log().body()
+                .body(authBody)
+                .contentType(JSON)
+                .when()
+                .post("https://reqres.in/api/login")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(200)
+                .extract().as(LoginResponseLombokModel.class);
+
+        assertEquals("QpwL5tke4Pnpja7X4", response.getToken());
+    }
+
+    @Test
+    void successfulLoginWithCustomAllureTest() {
+        LoginBodyLombokModel authBody = new LoginBodyLombokModel();
+        authBody.setEmail("eve.holt@reqres.in");
+        authBody.setPassword("cityslicka");
+
+        LoginResponseLombokModel response =  given()
+                .filter(withCustomTemplates())
+                .log().uri()
+                .log().method()
+                .log().body()
+                .body(authBody)
+                .contentType(JSON)
+                .when()
+                .post("https://reqres.in/api/login")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(200)
+                .extract().as(LoginResponseLombokModel.class);
+
+        assertEquals("QpwL5tke4Pnpja7X4", response.getToken());
+    }
+
+
+    @Test
+    void successfulLoginWithAllureStepsTest() {
+        LoginBodyLombokModel authBody = new LoginBodyLombokModel();
+        authBody.setEmail("eve.holt@reqres.in");
+        authBody.setPassword("cityslicka");
+
+        LoginResponseLombokModel response = step("Make login request", () ->
+            given()
+                    .filter(withCustomTemplates())
+                    .log().uri()
+                    .log().method()
+                    .log().body()
+                    .body(authBody)
+                    .contentType(JSON)
+                    .when()
+                    .post("https://reqres.in/api/login")
+                    .then()
+                    .log().status()
+                    .log().body()
+                    .statusCode(200)
+                    .extract().as(LoginResponseLombokModel.class));
+
+        step("Verify response", () ->
+            assertEquals("QpwL5tke4Pnpja7X4", response.getToken()));
+    }
+
 
 
 }
